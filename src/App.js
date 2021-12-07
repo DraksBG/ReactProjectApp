f98adcc436c1f2c96484969e77cb210f8e4fcb20
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-07 16:33:53 +0200

    Added routes
    Added routes, set up react Router and paths towards different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Components
+import { Navbar, Sidebar, Footer } from "./components";
+// Pages
+import {
+  Home,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  ErrorPage,
+  Checkout,
+  Private,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+     <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/products/:id" component={SingleProduct} />
+      <Route exact path="/checkout" component={Checkout} />
+      <Route exact path="/*" component={ErrorPage} />
+     </Switch>
+     <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import About from "./AboutPage";
+import ErrorPage from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+export {
+  Home,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  ErrorPage,
+  Checkout,
+  Private,
+};
