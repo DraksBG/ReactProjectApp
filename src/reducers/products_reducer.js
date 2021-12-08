da8e0668e8cdab5e2d4cc53ce61829089d17548d
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-08 17:16:01 +0200

    Fetching products
    Created Actions and state for when we are fetching products

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -7,23 +7,48 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
+} from "../actions";
 
 const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_OPEN) {
     return {
       ...state,
       isSidebarOpen: true,
-    }
+    };
   }
   if (action.type === SIDEBAR_CLOSE) {
     return {
       ...state,
       isSidebarOpen: false,
-    }
+    };
   }
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return {
+      ...state,
+      products_loading: true,
+    };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featuredProducts = action.payload.filter(
+      (product) => product.featured === true
+    );
 
-export default products_reducer
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featuredProducts,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return {
+      ...state,
+      products_loading: false,
+      products_error: true,
+    };
+  }
+  return state;
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default products_reducer;
