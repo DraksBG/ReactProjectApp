cfbc4c3b17df28399666faa3cdf417cdf3f1bc3d
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-08 15:42:35 +0200

    Update PageHero.js
    Added html and css markup and ass well redirect for the user

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-const PageHero = () => {
-  return <h4>page hero</h4>
-}
-
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { links } from "../utils/constants";
+const PageHero = ({ title }) => {
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>
+          <Link to="/">Home</Link>/ {title}
+        </h3>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   background: var(--clr-primary-10);
   width: 100%;
@@ -21,6 +29,6 @@ const Wrapper = styled.section`
   a:hover {
     color: var(--clr-primary-1);
   }
-`
+`;
 
-export default PageHero
+export default PageHero;
