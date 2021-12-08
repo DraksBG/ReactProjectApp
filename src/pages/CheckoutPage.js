9a3ec6f0f3e1a3befb378cfd98c8d86fcc5694c2
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-08 15:44:22 +0200

    Update CheckoutPage.js

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
+import React from "react";
+import styled from "styled-components";
+import { PageHero, StripeCheckout } from "../components";
 // extra imports
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
-}
-const Wrapper = styled.div``
-export default CheckoutPage
+  return (
+    <main>
+      <PageHero title="Checkout" />
+      <Wrapper page='page'></Wrapper>
+    </main>
+  );
+};
+const Wrapper = styled.div``;
+export default CheckoutPage;
