9e83ca246d9840da0beb9eb33225cc4c1650bbe5
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-08 15:32:57 +0200

    Update AboutPage.js
    Added html markup and classes to the page as well as text and pictures

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="aboutPage" />
+        <article>
+          <div className="title">
+            <h2>How it all began</h2>
+            <div className="underline"></div>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +47,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
