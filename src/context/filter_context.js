a026f6bdc298b4d283a034645c31a50c755019a5
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-10 00:36:48 +0200

    Initial logic for sorting

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,6 +16,7 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: true,
+  sort: 'price-lowest',
 }
 
 const FilterContext = React.createContext()
@@ -34,9 +35,15 @@ export const FilterProvider = ({ children }) => {
   const setListView = () => {
     dispatch({ type: SET_LISTVIEW })
   }
+  const updateSort = (e) => {
 
+    const value = e.target.value
+    dispatch({ type: UPDATE_SORT, payload: value })
+  }
   return (
-    <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, updateSort }}
+    >
       {children}
     </FilterContext.Provider>
   )
