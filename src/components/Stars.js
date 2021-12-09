54664b447dd723d48de207a88150ed70a024cb31
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-09 16:54:07 +0200

    Created Starts component

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from "react";
+import styled from "styled-components";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+const Stars = ({ stars, reviews }) => {
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    if (index + 1 <= stars) {
+      return <BsStarFill />;
+    } else if (index + 1 > stars && index + 1 <= stars + 0.5) {
+      return <BsStarHalf />;
+    } else {
+      return <BsStar />;
+    }
+  });
+  return (
+    <Wrapper>
+      <div className="stars"><span>{tempStars}</span></div>
+      <p className="reviews">({reviews} customer reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -18,5 +32,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
