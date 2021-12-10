830d7442e847f11bff728125fc32decb61a62149
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-10 15:05:38 +0200

    updated the functionality
    Updated the functionality of both cart context and cart reducer

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -6,6 +6,7 @@ import {
   TOGGLE_CART_ITEM_AMOUNT,
   CLEAR_CART,
   COUNT_CART_TOTALS,
+  CLEAR_FILTERS,
 } from '../actions'
 
 const getLocalStorage = () => {
@@ -34,15 +35,21 @@ export const CartProvider = ({ children }) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
   }
   // remove item
-  const removeItem = (id) => {}
-
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id })
+  }
   // toggle amount
-  const toggleAmount = (id, value) => {}
-
+  const toggleAmount = (id, value) => {
+    console.log(id, value)
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } })
+  }
   // clear cart
-  const clearCart = () => {}
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
 
   useEffect(() => {
+    dispatch({ type: COUNT_CART_TOTALS })
     localStorage.setItem('cart', JSON.stringify(state.cart))
   }, [state.cart])
 
