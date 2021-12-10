539ce0adb6962e58bfb14f78544138a86d103b3a
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-10 23:25:04 +0200

    Created stripe component

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,29 +1,149 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { loadStripe } from "@stripe/stripe-js";
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { loadStripe } from '@stripe/stripe-js'
 import {
   CardElement,
   useStripe,
   Elements,
   useElements,
-} from "@stripe/react-stripe-js";
-import axios from "axios";
-import { useCartContext } from "../context/cart_context";
-import { useUserContext } from "../context/user_context";
-import { formatPrice } from "../utils/helpers";
-import { useHistory } from "react-router-dom";
+} from '@stripe/react-stripe-js'
+import axios from 'axios'
+import { useCartContext } from '../context/cart_context'
+import { useUserContext } from '../context/user_context'
+import { formatPrice } from '../utils/helpers'
+import { useHistory } from 'react-router-dom'
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
 
 const CheckoutForm = () => {
-  return <h2>hello from Stripe Checkout </h2>;
-};
+  const { cart, total_amount, shipping_fee, clearCart } = useCartContext()
+  const { curUser } = useUserContext()
+  const history = useHistory()
+  // STRIPE STUFF
+  const [succeeded, setSucceeded] = useState(false)
+  const [error, setError] = useState(null)
+  const [processing, setProcessing] = useState('')
+  const [disabled, setDisabled] = useState(true)
+  const [clientSecret, setClientSecret] = useState('')
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: '#32325d',
+        fontFamily: 'Arial, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+          color: '#32325d',
+        },
+      },
+      invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a',
+      },
+    },
+  }
+
+  const createPaymentIntent = async () => {
+    try {
+      const { data } = await axios.post(
+        '/.netlify/functions/create-payment-intent',
+        JSON.stringify({ cart, shipping_fee, total_amount })
+      )
+
+      setClientSecret(data.clientSecret)
+    } catch (error) {
+      // console.log(error.response)
+    }
+  }
+
+  useEffect(() => {
+    createPaymentIntent()
+    // eslint-disable-next-line
+  }, [])
+
+  const handleChange = async (event) => {
+    setDisabled(event.empty)
+    setError(event.error ? event.error.message : '')
+  }
+  const handleSubmit = async (ev) => {
+    ev.preventDefault()
+    setProcessing(true)
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+      },
+    })
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false)
+    } else {
+      setError(null)
+      setProcessing(false)
+      setSucceeded(true)
+      setTimeout(() => {
+        clearCart()
+        history.push('/')
+      }, 10000)
+    }
+  }
+
+  return (
+    <div>
+      {succeeded ? (
+        <article>
+          <h4>Thank you</h4>
+          <h4>Your payment was successful!</h4>
+          <h4>Redirecting to home page shortly</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {curUser && curUser.name}</h4>
+          <p>Your total is {formatPrice(shipping_fee + total_amount)}</p>
+          <p>Test Card Number : 1234 5678 8765 1579</p>
+        </article>
+      )}
+      <form id='payment-form' onSubmit={handleSubmit}>
+        <CardElement
+          id='card-element'
+          options={cardStyle}
+          onChange={handleChange}
+        />
+        <button disabled={processing || disabled || succeeded} id='submit'>
+          <span id='button-text'>
+            {processing ? <div className='spinner' id='spinnier'></div> : 'Pay'}
+          </span>
+        </button>
+        {/* Show any error that happens when processing the payment */}
+        {error && (
+          <div className='card-error' role='alert'>
+            {error}
+          </div>
+        )}
+        {/* Show  a success message upon completion */}
+        <p className={succeeded ? 'result-message' : 'result-message hidden'}>
+          Payment succedded, see the result in your
+          <a href={`https://dashboard.stripe.com/test/payments`}>
+            Stripe dasboard.
+          </a>
+          Refresh the page to pay again
+        </p>
+      </form>
+    </div>
+  )
+}
 
 const StripeCheckout = () => {
   return (
     <Wrapper>
-      <CheckoutForm />
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.section`
   form {
@@ -122,7 +242,7 @@ const Wrapper = styled.section`
   .spinner:before,
   .spinner:after {
     position: absolute;
-    content: "";
+    content: '';
   }
   .spinner:before {
     width: 10.4px;
@@ -163,6 +283,6 @@ const Wrapper = styled.section`
       width: 80vw;
     }
   }
-`;
+`
 
-export default StripeCheckout;
+export default StripeCheckout
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,36 +1,37 @@
-import React from "react";
-import styled from "styled-components";
-import { PageHero, StripeCheckout } from "../components";
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero, StripeCheckout } from '../components'
+
 // extra imports
-import { useCartContext } from "../context/cart_context";
-import { Link } from "react-router-dom";
+import { useCartContext } from '../context/cart_context'
+import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
-  const { cart } = useCartContext();
+  const { cart } = useCartContext()
   return (
     <main>
-      <PageHero title="Checkout" />
-      <Wrapper page="page">
-        {cart.length > 0 ? (
-          <StripeCheckout />
-        ) : (
-          <div className="empty">
+      <PageHero title='checkout' />
+      <Wrapper className='page'>
+        {cart.length < 1 ? (
+          <div className='empty'>
             <h2>your cart is empty</h2>
-            <Link to="/products" className="btn">
+            <Link to='/products' className='btn'>
               fill it
             </Link>
           </div>
+        ) : (
+          <StripeCheckout />
         )}
       </Wrapper>
     </main>
-  );
-};
+  )
+}
 const Wrapper = styled.div`
-  dispaly: flex;
+  display: flex;
   align-items: center;
   justify-content: center;
   .empty {
     text-align: center;
   }
-`;
-export default CheckoutPage;
+`
+export default CheckoutPage
