aa92fd68271d69ece256230c6c156896f8159d51
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-10 15:03:53 +0200

    Set up cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,8 +4,38 @@ import { formatPrice } from '../utils/helpers'
 import AmountButtons from './AmountButtons'
 import { FaTrash } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
-const CartItem = () => {
-  return <h4>cart item</h4>
+const CartItem = ({ id, image, name, color, price, amount }) => {
+  const { removeItem, toggleAmount } = useCartContext()
+  const increase = () => {
+    toggleAmount(id, 'inc')
+  }
+  const decrease = () => {
+    toggleAmount(id, 'dec')
+  }
+  return (
+    <Wrapper>
+      <div className='title'>
+        <img src={image} alt={name} />
+        <div>
+          <h5 className='name'>{name}</h5>
+          <p className='color'>
+            color : <span style={{ background: color }}></span>
+          </p>
+          <h5 className='price-small'>{formatPrice(price)}</h5>
+        </div>
+      </div>
+      <h5 className='price'>{formatPrice(price)}</h5>
+      <AmountButtons amount={amount} increase={increase} decrease={decrease} />
+      <h5 className='subtotal'>{formatPrice(price * amount)}</h5>
+      <button
+        type='button'
+        className='remove-btn'
+        onClick={() => removeItem(id)}
+      >
+        <FaTrash />
+      </button>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.article`
@@ -23,7 +53,7 @@ const Wrapper = styled.article`
   margin-bottom: 3rem;
   align-items: center;
   .title {
-    grid-template-rows: 75px;
+    height: 100%;
     display: grid;
     grid-template-columns: 75px 125px;
     align-items: center;
