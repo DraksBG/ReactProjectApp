e7d2e6bc0823600577ab506e6cac90c67c37f1cf
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-10 15:00:11 +0200

    Update CarContent.js
    Inital set up for the cart content component

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -7,8 +7,31 @@ import CartItem from './CartItem'
 import CartTotals from './CartTotals'
 
 const CartContent = () => {
-  return <h4>cart content </h4>
+  const { cart, clearCart } = useCartContext()
+  return (
+    <Wrapper className='seciton section-center'>
+      <CartColumns />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />
+      })}
+      <hr />
+      <div className='link-container'>
+        <Link to='/products' className='link-btn'>
+          continue shopping
+        </Link>
+        <button
+          type='button'
+          className='link-btn clear-btn'
+          onClick={clearCart}
+        >
+          clear shopping cart
+        </button>
+      </div>
+      <CartTotals />
+    </Wrapper>
+  )
 }
+
 const Wrapper = styled.section`
   .link-container {
     display: flex;
