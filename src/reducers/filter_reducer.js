562044d9c3a42198528cb491970b13eb87c5bef4
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-12-10 14:39:34 +0200

    Update filter_reducer.js
    Added full filters functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -64,8 +64,44 @@ const filter_reducer = (state, action) => {
     return { ...state, filters: { ...state.filters, [name]: value } }
   }
   if (action.type === FILTER_PRODUCTS) {
-    console.log('filtering products')
-    return { ...state }
+    const { all_products } = state
+    const { text, category, company, color, price, shipping } = state.filters
+
+    let tempProducts = [...all_products]
+    // filtering
+    // text
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text)
+      })
+    }
+    // category
+    if (category !== 'all') {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      )
+    }
+
+    // company
+    if (company !== 'all') {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      )
+    }
+    // colors
+    if (color !== 'all') {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color)
+      })
+    }
+    // price
+    tempProducts = tempProducts.filter((product) => product.price <= price)
+    // shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter((product) => product.shipping === true)
+    }
+
+    return { ...state, filtered_products: tempProducts }
   }
   if (action.type === CLEAR_FILTERS) {
     return {
